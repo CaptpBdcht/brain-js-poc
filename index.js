ec049006ed6a79cd4fd381a01ff259ffdb01807b
Captp <vincent_mot@hotmail.fr>
2018-01-30 21:30:52 +0100

    Added v1.0 roadmap

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+/**
+ * ARGV
+ * [fichier dataset entrainement]
+ * optional 
+ *      -t, --train [fichier de sortie]
+ *      -p, --predict [fichier dataset inputs] [fichier de sortie]
+ * }
+ * 
+ * Par défaut, avec un seul argument, test de la précision d'un
+ * réseau (par division du dataset en 2).
+ * Avec -t ou --train, entrainement d'un brain.NeuralNetwork et
+ * enregistrement au format JSON.
+ * Avec -p ou --predict, entrainement d'un NN, test des inputs 
+ * et enregistrement des sorties au format JSON.
+ * 
+ * DATASET
+ * [ ... { input: Object || Array, output: Object || Array } ]
+ */
+
 const core = require('./core');
 const argvToOpts = require('./argv-to-opts');
 const showResults = require('./utils').showResults;
@@ -14,12 +33,3 @@ argvToOpts()
 // // Test
 // const netAccuracy = accuracy(net, DATA);
 // console.log('Accuracy:', rateToPercentage(netAccuracy), '%');
-
-/**
- * But
- * -> Entrainer un réseau
- * -> Tester la précision d'un réseau
- * -> Faire des prédictions avec un réseau
- * Entrée données
- * -> dataFilename
- */
