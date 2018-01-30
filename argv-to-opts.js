344772042bedb55231d0ba31557aa8ddac700938
Captp <vincent_mot@hotmail.fr>
2018-01-30 21:39:53 +0100

    Processing args

--- argv-to-opts.js
+++ argv-to-opts.js
@@ -1,12 +1,24 @@
-module.exports = () => {
+function processArgs(args) {
     return new Promise((resolve, reject) => {
-        if (process.argv.length < 3)
-            reject('Missing dataFilename');
-
-        const CORE_OPTS = {
-            'dataFile': process.argv[2]
-        };
+        const NB_ARGS = args.length;
+        const CORE_OPTS = {};
+        
+        switch (NB_ARGS) {
+            case 6:
+                CORE_OPTS['inputsFile'] = process.argv[5];
+            case 5:
+                CORE_OPTS['outputFile'] = process.argv[4];
+            case 3:
+                CORE_OPTS['dataFile'] = process.argv[2];
+                break;
+            default:
+                reject('Invalid args');
+        }
 
         resolve(CORE_OPTS);
     });
+}
+
+module.exports = () => {
+    return processArgs(process.argv);
 };
--- index.js
+++ index.js
@@ -3,15 +3,15 @@
  * [fichier dataset entrainement]
  * optional 
  *      -t, --train [fichier de sortie]
- *      -p, --predict [fichier dataset inputs] [fichier de sortie]
+ *      -p, --predict [fichier de sortie] [fichier dataset inputs]
  * }
  * 
  * Par défaut, avec un seul argument, test de la précision d'un
  * réseau (par division du dataset en 2).
  * Avec -t ou --train, entrainement d'un brain.NeuralNetwork et
  * enregistrement au format JSON.
- * Avec -p ou --predict, entrainement d'un NN, test des inputs 
- * et enregistrement des sorties au format JSON.
+ * Avec -p ou --predict, entrainement d'un NN, test des inputs et
+ * enregistrement des sorties au format JSON.
  * 
  * DATASET
  * [ ... { input: Object || Array, output: Object || Array } ]
